2eae41d0f122d6914e2b290d83b26fc544b726e8
Hle1801 <hoang.lv184870@gmail.com>
2024-06-13 17:16:46 +0700

    1306

--- src/main/java/hseneca/personal_website/dto/UserDTO.java
+++ src/main/java/hseneca/personal_website/dto/UserDTO.java
@@ -1,13 +1,13 @@
-package hseneca.personal_website.dto;
-
-import lombok.Data;
-
-import java.time.ZonedDateTime;
-
-@Data
-public class UserDTO extends BaseDTO{
-    private String username;
-    private String password;
-    private Integer age;
-    private String school;
-}
+//package hseneca.personal_website.dto;
+//
+//import lombok.Data;
+//
+//import java.time.ZonedDateTime;
+//
+//@Data
+//public class UserDTO extends BaseDTO{
+//    private String username;
+//    private String password;
+//    private Integer age;
+//    private String school;
+//}
--- src/main/java/hseneca/personal_website/service/impl/LoginServiceImpl.java
+++ src/main/java/hseneca/personal_website/service/impl/LoginServiceImpl.java
@@ -1,13 +1,14 @@
 package hseneca.personal_website.service.impl;
 
-import hseneca.personal_website.dto.UserDTO;
+
+import a8seneca.test.dto.UserDTO;
 import hseneca.personal_website.model.request.SignupRequest;
 
 import java.util.List;
+
 
 public interface LoginServiceImpl {
     List<UserDTO> getAllUser();
     boolean checkLogin(String userName, String password);
     boolean addUser(SignupRequest singupRequest);
-
 }
